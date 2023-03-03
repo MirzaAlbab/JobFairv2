f82365ec2f5d79950aa7f26e3fa3ce87a47c59db
unknown <mirzaalbabfirdaus17@gmail.com>
2023-03-03 15:08:41 +0700

    add photo field

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'about'=> '',
             'address' =>'',
-            'email' => 'admin@gmail.com',
+            'email' => 'mirzaalbabfirdaus17@gmail.com',
             'phone' => '',
             'password'=> '$2y$10$Fz4h8C5JDHNPJ3zb4rq/7epCGnoIj5kpE.Hzdq/1D67MQeH8l/o0m',
             'role' => 'admin',
