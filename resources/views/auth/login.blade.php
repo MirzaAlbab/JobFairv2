559f852a8cbcfc4daacb46c1dc3043cf5e0587d0
unknown <mirzaalbabfirdaus17@gmail.com>
2023-06-27 22:35:33 +0700

    update login and register view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,22 +32,31 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
+           
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+                <div class="justify-self-start"> 
+                    <a class="text-sm text-blue-600 hover:text-blue-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot password?') }}
+                    </a>
+                </div>
             @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+           
+            <div class="justify-self-start">
+                <x-primary-button class="ml-3 flex-1 flex-end">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
+           
             
         </div>
         <div class="flex items-center justify-center mt-4">
             
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-                {{ __('Don\'t have an account? Register' ) }}
+            <p class="text-sm text-gray-600 hover:text-gray-900 rounded-md">
+                {{ __('Don\'t have an account?') }}
+            </p>
+            <a class="ml-1 text-sm text-blue-600 hover:text-blue-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Register' ) }}
             </a>
         </div>
     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -59,14 +59,22 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="flex items-center justify-between mt-4">
+            <div class="justify-self-start"> 
+                <p class="inline-flex text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already have account?') }}
+                </p>
+                <a class="ml-1 text-sm text-blue-600 hover:text-blue-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                    {{ __('Login') }}
+                </a>
+            </div>
+           
+            <div class="justify-self-start"> 
+                <x-primary-button class="ml-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
+           
         </div>
     </form>
 </x-guest-layout>
