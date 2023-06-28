3e42ba6fd7563b2370301b16093aad4cd384bc11
unknown <mirzaalbabfirdaus17@gmail.com>
2023-06-28 11:07:59 +0700

    update navigation add img profile

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -92,6 +92,11 @@
         @endcan
       <li class="nav-item dropdown pe-3">
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+          @if(Auth::user()->photo)
+          <img src=" {{ asset('storage/'.Auth::user()->photo) }} " alt="Profile" class="rounded-circle">
+          @else
+          <img src=" {{ asset('assets/img/profile-img.jpg') }} " alt="Profile" class="rounded-circle">
+          @endif
           <span class="dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
         </a><!-- End Profile Name Icon -->
        
